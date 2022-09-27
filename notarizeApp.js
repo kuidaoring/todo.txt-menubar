dc14c30338f1311c3fd61ae4720cb2791db47340
kuidaoring <kuijob@gmail.com>
2022-09-28 01:29:07 +0900

    公証の設定を修正

--- notarizeApp.js
+++ notarizeApp.js
@@ -3,10 +3,12 @@ const { notarize } = require("electron-notarize");
 
 exports.default = async function notarizeApp(context) {
   await notarize({
-    tool: "notarytool",
-    appPath: `${context.appOutDir}/${context.packager.appInfo.productFileName}`,
+    tool: process.env.CI === "true" ? "notarytool" : "legacy",
+    appBundleId: "com.gihub.kuidaoring.todotxt-menubar",
+    ascProvider: process.env.TEAM_ID, // for legacy
+    teamId: process.env.TEAM_ID, // for notarytool
+    appPath: "dist/mac/todo.txt-menubar.app",
     appleId: process.env.APPLE_ID,
-    appleIdPassword: process.env.APPLE_ID_PASSWORD,
-    teamId: process.env.TEAM_ID,
+    appleIdPassword: process.env.APPLE_ID_PASS,
   });
 };
